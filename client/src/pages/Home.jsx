442f1440ee4f52ac77f291faef1c2b8c1f603bd0
bryce <bgt17@txstate.edu>
2024-01-26 18:57:16 -0600

    Header added, Tailwind css+frostbite added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Button } from 'flowbite-react'
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="flex flex-wrap gap-2">
+      <Button pill>Default</Button>
+      <Button color="blue" pill>
+        Blue
+      </Button>
+      <Button color="gray" pill>
+        Gray
+      </Button>
+      <Button color="dark" pill>
+        Dark
+      </Button>
+      <Button color="light" pill>
+        Light
+      </Button>
+      <Button color="success" pill>
+        Success
+      </Button>
+      <Button color="failure" pill>
+        Failure
+      </Button>
+      <Button color="warning" pill>
+        Warning
+      </Button>
+      <Button color="purple" pill>
+        Purple
+      </Button>
+    </div>
+  );
 }
