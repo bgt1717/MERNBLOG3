07a26ae92b399e4245338c4e5590f6668f9b2f6a
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-01-29 13:17:07 -0600

    Header updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { Navbar } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from 'react-icons/ai';
 import { Button } from 'flowbite-react';
 import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
+  const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
       {/* SMALL AND ABOVE: sm:aaaaa */}
@@ -13,7 +14,7 @@ export default function Header() {
         Blog
       </Link>
       <form>
-        <input
+        <TextInput
           type='text'
           placeholder='Search...'
           // You can't directly pass a component as a prop, so we need to render it
@@ -25,13 +26,27 @@ export default function Header() {
       <Button className='w-12 h-10 lg:hidden' color='gray' pill >
         <AiOutlineSearch/>
       </Button>
-      <div className="">
+      <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color='gray' pill>
             <FaMoon />
         </Button>
         <Link to='/sign-in'></Link>
-
+        <Button gradientDuoTone='purpleToBlue' >
+          Sign In
+        </Button>
+        <Navbar.Toggle />
       </div>
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={'div'}>
+            <Link to="/"> Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"} as={'div'}>
+            <Link to='/about'> about</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"} as={'div'}>
+            <Link to='/projects'>Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
     </Navbar>
   );
 }
