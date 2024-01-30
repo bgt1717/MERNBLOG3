d60ab97c1ad73dbb92512ad9129c8cc669ee6265
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-01-29 22:22:43 -0600

    Test API route added.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+
 
 dotenv.config();
 
@@ -18,4 +20,4 @@ app.listen(3000, () => {
   console.log("Server running on port 3000.");
 });
 
-//yIqTlOdypddba8KQ
+app.use('/api/user', userRoutes);
