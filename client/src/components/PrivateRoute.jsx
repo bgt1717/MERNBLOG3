eead01d0c9e16509aac34b537652f677da3b49e9
bryce <bgt17@txstate.edu>
2024-02-28 18:17:13 -0600

    privateRoute comment added

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,6 +3,6 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  // if the currentUser is true, return children which is dashboard <Dashboard/> : <SignIn />
+  // if the currentUser is true, return children which is dashboard <Dashboard/> : <SignIn />, see App.jsx for it to make more sense. 
   return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
 }
