b4874d4c7d60ec94372f215330d78619910f855d
bryce <bgt17@txstate.edu>
2024-03-07 18:58:51 -0600

    Added Signout functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -5,7 +5,7 @@ import User from '../models/user.model.js';
 export const test = (req, res) => {
   res.json({ message: 'API is working!' });
 };
-
+// next handles errors. 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to update this user'));
@@ -61,6 +61,17 @@ export const deleteUser = async (req, res, next) => {
   try {
     await User.findByIdAndDelete(req.params.userId);
     res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signout = (req, res, next) => {
+  try {
+    res
+      .clearCookie('access_token')
+      .status(200)
+      .json('User has been signed out');
   } catch (error) {
     next(error);
   }
