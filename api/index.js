990e7266d61c868633554cddc211f7ca6ca60e68
bryce <bgt17@txstate.edu>
2024-03-09 16:56:34 -0600

    create a post api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import postRoutes from './routes/post.route.js';
 
 
 dotenv.config();
@@ -27,6 +28,7 @@ app.listen(3000, () => {
 //adds an s at the end of user in MongoDB. 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 
 //Error Middleware
 app.use((err, req, res, next) => {
