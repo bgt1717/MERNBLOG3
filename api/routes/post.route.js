46d078cd57fe56001b88d0450e2dd0d65e59f5d4
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-03-12 14:23:45 -0500

    add Delete Post Functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, getposts } from '../controllers/post.controller.js';
+import { create, deletepost, getposts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
 router.get('/getposts', getposts)
+// '/deletepost/:postId/:userId': This is the route path pattern. It specifies that this route will match requests to a URL that looks like /deletepost/somePostId/someUserId, where :postId and :userId are placeholders for actual values that will be provided in the request.
+// verifyToken: This is a middleware function that likely verifies the authenticity of a user's token before allowing access to the route.
+// deletepost: This is the name of the function that will handle requests to this route. It's assumed to be defined elsewhere in the code.
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 
 
 export default router;
