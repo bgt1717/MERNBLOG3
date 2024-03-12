d909457c9191495dd46bc79a3e2c20109ccd4b42
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-03-12 15:11:31 -0500

    added update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, deletepost, getposts } from '../controllers/post.controller.js';
+import { create, deletepost, getposts, updatepost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
@@ -10,6 +10,8 @@ router.get('/getposts', getposts)
 // verifyToken: This is a middleware function that likely verifies the authenticity of a user's token before allowing access to the route.
 // deletepost: This is the name of the function that will handle requests to this route. It's assumed to be defined elsewhere in the code.
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 
 export default router;
