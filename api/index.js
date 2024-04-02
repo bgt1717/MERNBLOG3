de6c2239e68afbb17c9c715f0cc11894436c6f8a
bgt1717 <bgt17@txtstate.edu>
2024-04-01 21:17:23 -0500

    comment section added to post page

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import path from 'path';
 
 
@@ -33,6 +34,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 //runs dist file after it is built.
 app.use(express.static(path.join(__dirname, '/client/dist')));
